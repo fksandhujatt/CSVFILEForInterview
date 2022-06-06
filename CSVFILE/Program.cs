e70715dc11d9442a41d7eebc04f7ca4b854d06ad
Fahad Khalil <fahadkk09@hotmail.com>
2022-06-06 05:17:04 +0100

    Remove unwanted comments

--- CSVFILE/Program.cs
+++ CSVFILE/Program.cs
@@ -16,9 +16,7 @@ namespace CSVFILE
         }
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
             string csv_file_path = @"G:\CSVFileCSharp\CSV-Import-to-Database-Csharp\stock_list.csv";
-            //string csv_file_path = @"G:\CSVFileCSharp\CSV-Import-to-Database-Csharp\DB.txt";
 
             string ext = Path.GetExtension(csv_file_path);
             bool fileExist = File.Exists(csv_file_path);
@@ -36,23 +34,7 @@ namespace CSVFILE
                 {
                     ReadCSV ReadCSV = new ReadCSV();
                     InsertDatatoDB InsertDatatoDB = new InsertDatatoDB();
-                    //List<Stock> Stock = ReadCSV.getDataFromCSVFile(csv_file_path);
                     List<Stock> Stock = ReadCSV.getDataFromCSVFile2(csv_file_path);
-                    //InsertDatatoDB.insertDatatoDBStock(Stock);
-                    // Create new stopwatch
-                    //Stopwatch stopwatch = new Stopwatch();
-
-                    //// Begin timing
-                    //stopwatch.Start();
-
-                    //System.Threading.Thread.Sleep(500);
-
-
-
-                    //// Stop timing
-                    //stopwatch.Stop();
-
-                    //Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
                     if (Stock.Count > 0)
                     {
                         InsertDatatoDB.insertDatatoDBStock2(Stock);
@@ -61,15 +43,7 @@ namespace CSVFILE
                     {
                         Console.WriteLine("File has no Data");
                     }
-
-                    //GetStockList GetStockList = new GetStockList();
-                    //List<Stock> stocks = GetStockList.getStockList();
-                    //foreach (var item in stocks)
-                    //{
-                    //    Console.WriteLine("{0} {1} {2}", item.Product, item.ProductCode, item.Price);
-                    //}
                     Console.WriteLine("Data Saved Successfully");
-
                 }
             }
             else
